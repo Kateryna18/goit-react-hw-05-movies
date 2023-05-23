5096b6c033ae1b390b9b30ca0872f1be16872f22
Kateryna18 <mikolaichukkate@icloud.com>
2023-05-23 20:53:07 +0300

    added fucntion prepared info for render

--- src/pages/Moviesdetails.jsx
+++ src/pages/Moviesdetails.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState, Suspense } from 'react';
 import { Link, Outlet, useParams, useLocation } from 'react-router-dom';
 import { fetchMovieDetails } from 'api/api';
 import { ColorRing } from 'react-loader-spinner';
-import css from './Moviesdetails.module.css'
+import css from './Moviesdetails.module.css';
 
 export default function Moviesdetails() {
   const [movieInfo, setMovieInfo] = useState({});
@@ -12,14 +12,32 @@ export default function Moviesdetails() {
   useEffect(() => {
     const getMovieInfo = async () => {
       const movieInfo = await fetchMovieDetails(movieId);
-      setMovieInfo(movieInfo);
+      setMovieInfo(preparedMovieInfo(movieInfo));
     };
 
     getMovieInfo();
   }, [movieId]);
 
-  const { poster_path, title, vote_count, overview } = movieInfo;
+  const preparedMovieInfo = ({
+    poster_path,
+    title,
+    vote_average,
+    overview,
+    genres,
+  }) => {
+    const score = vote_average.toFixed(1);
+    const genresMovie = genres.map(genre => genre.name).join(', ');
 
+    return {
+      posterPath: poster_path,
+      score: score,
+      title: title,
+      overview: overview,
+      genres: genresMovie,
+    };
+  };
+
+  const { posterPath, title, score, overview, genres } = movieInfo;
   return (
     <Suspense
       fallback={
@@ -35,33 +53,37 @@ export default function Moviesdetails() {
       }
     >
       <section>
-        <Link className={css.button} to={location.state}>Go Back</Link>
+        <Link className={css.button} to={location.state}>
+          Go Back
+        </Link>
         {Object.keys(movieInfo).length && (
           <div className={css.card}>
             <img
               className={css.image}
-              src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+              src={`https://image.tmdb.org/t/p/w500${posterPath}`}
               alt={title}
             />
             <div className={css.info}>
               <div>
-              <h2 className={css.title}>{title}</h2>
-            <p className={css.score}>User score: {vote_count}%</p>
-            <h3 className={css.overviewTitle}>Overview</h3>
-            <p className={css.overviewText}>{overview}</p>
-            <h4 className={css.genres}>Genres</h4>
-            <p></p>
+                <h2 className={css.title}>{title}</h2>
+                <p className={css.score}>User score: {score}%</p>
+                <h3 className={css.overviewTitle}>Overview</h3>
+                <p className={css.overviewText}>{overview}</p>
+                <h4 className={css.genres}>Genres</h4>
+                <p>{genres}</p>
               </div>
-            <div>
-            <ul className={css.list}>
-          <li className={css.item}>
-            <Link className={css.link} to="cast">Cast</Link>
-          </li>
-          <li className={css.item}>
-            <Link className={css.link} to="reviews">Reviews</Link>
-          </li>
-        </ul>
-            </div>
+              <ul className={css.list}>
+                <li className={css.item}>
+                  <Link className={css.link} to="cast">
+                    Cast
+                  </Link>
+                </li>
+                <li className={css.item}>
+                  <Link className={css.link} to="reviews">
+                    Reviews
+                  </Link>
+                </li>
+              </ul>
             </div>
           </div>
         )}
